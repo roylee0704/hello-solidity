1e24de7ffd8fae9c009f47148f0131227a5af786
roylee0704 <roylee0704@gmail.com>
2022-07-29 10:18:19 +0700

    feat: add more functions

--- SendMoneyExample.sol
+++ SendMoneyExample.sol
@@ -4,11 +4,24 @@ contract SendMoneyExample {
     uint256 public balanceReceived;
 
     function receiveMoney() public payable {
+        // msg is a global always-existing object.
+        // - .value
+        // - .sender
         balanceReceived += msg.value;
     }
 
+    // query function: foc
     function getBalance() public view returns (uint256) {
-        // ths
+        // address: refers to contract-account's address
         return address(this).balance;
+    }
+
+    function getSender() public view returns (address) {
+        // .sender refers to externally-owned-account(EOA) 's address
+        return msg.sender;
+    }
+
+    function getSenderBalance() public view returns (uint256) {
+        return msg.sender.balance;
     }
 }
